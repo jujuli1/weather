c7466ae71bd2e7055246e8fdeb45acfd3a681dfe
LEFEVRE <lefevrejulien@live.fr>
2024-12-08 22:59:25 +0100

    style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import * as THREE from 'three';
+import { color } from 'three/webgpu';
 
-
+{/** génération aléatoire facon matrix */}
+const generateRandomDelay = () => {
+  // Génère une valeur aléatoire entre 1 et 3 secondes
+  return `${Math.random() * 2 + 1}s`;
+};
 
 const api = {
   key: '2f454eb9b1d7dad726b298cd0e372342',
@@ -14,6 +19,18 @@ function App() {
   const [weather, setWeather] = useState({});
   const [error, setError] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const elements = Array.from({ length: 5 }, (_, i) => ({
+    text: `0 1 00${i + 1}`,
+    delay: generateRandomDelay(), // Assigne un délai aléatoire à chaque élément
+  }));
+
+
+  /*Lever/coucher du soleil
+  const sunrise = new Date(weather.sys.sunrise * 1000).toLocaleTimeString();
+  const sunset = new Date(weather.sys.sunset * 1000).toLocaleTimeString();
+*/
+
 
   const searchPressed = () => {
     fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
@@ -37,12 +54,13 @@ function App() {
   useEffect(() => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+    
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.getElementById("three-container").appendChild(renderer.domElement);
 
     // Moon
-    const geometry = new THREE.SphereGeometry(0.6, 64, 64)
+    const geometry = new THREE.SphereGeometry(0.3, 32, 32)
     const texture = new THREE.TextureLoader().load('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5MQZS7B0LbbZ4LJgK-djWwW3oczycATcJ3w&s', () => {
       console.log("Texture chargée avec succès");
   }, undefined, (err) => {
@@ -50,10 +68,10 @@ function App() {
   });
     const material = new THREE.MeshPhongMaterial({map: texture})
     const cube = new THREE.Mesh(geometry, material);
-    const ambientLight = new THREE.AmbientLight(0xffffff,3);
+    const ambientLight = new THREE.AmbientLight(0xffffff,0.5);
     scene.add(ambientLight);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 4);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLight.position.set(5, 10, 7.5);
     scene.add(directionalLight);
     scene.add(cube);
@@ -62,9 +80,10 @@ function App() {
     // Caméra
     camera.position.z = 5;
 
+
     // Variables pour le mouvement circulaire
     let angle = 0; // Angle initial en radians
-    const radius = 3; // Rayon du cercle
+    const radius = 4; // Rayon du cercle
     const speed = 0.001; // Vitesse de rotation (radians par frame)
 
     // Animation
@@ -114,17 +133,42 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="meteok">METEOK</h1>
+      {elements.map((el, index) => (
+      <div className='containeur'>
+      
+      <h1 key={index} style={{ animationDelay: el.delay }} className="meteok">{el.text}</h1>
+      
+    
+      </div>))}
+      <div className='containeur2'>
+      {elements.map((el, index) => (
+      <div className='containeur'>
+      
+      <h1 key={index} style={{ animationDelay: el.delay }} className="meteok">{el.text}</h1>
+      <h1 key={index} style={{ animationDelay: el.delay }} className="meteok">{el.text}</h1>
+      
+    
+      </div>))}
+      <h1 className="meteok">0<br/>0<br/>*<br/>0</h1>
+      <h1 className="meteok">0<br/>1<br/>1<br/>1</h1>
+      </div>
+
+      <div className='containeur3'>
+      
+      <h1 className="meteok">0<br/>0<br/>|<br/>1</h1>
+      <h1 className="meteok">0<br/>0<br/>|<br/>1</h1>
+      </div>
+      
 
       <header className="App-header">
         <input
           className="recherche"
           type="text"
-          placeholder="Rechercher..."
+          placeholder="Ville..."
           onChange={(e) => setSearch(e.target.value)}
         />
         <button className="btnR" onClick={searchPressed}>
-          Recherche
+          Meteok
         </button>
       </header>
 
@@ -132,22 +176,39 @@ function App() {
        {isModalOpen && (
         <div className="modal">
           <div className="modal-content">
+            
             <button
               className="modal-close"
-              onClick={() => setIsModalOpen(false)} // Fermer la modale
+              onClick={() => setIsModalOpen(false)} // cloz
             >
               X
             </button>
-            {typeof weather.main !== 'undefined' ? (
-              <div>
-                <p>{weather.name}</p>
-                <p>{weather.main.temp}°C</p>
-                <p>{weather.weather[0].main}</p>
-                <p>({weather.weather[0].description})</p>
-              </div>
+
+            
+
+              {typeof weather.main !== 'undefined' ? (
+              
+             <div style={{ border: '1px solid #ddd', padding: '20px', borderRadius: '10px', background: '#000000' }}>
+             <h3 style={{ color: '#2c3e50' }}>Météo à {weather.name}, {weather.sys.country}</h3>
+
+            <ol>
+            <li><h3 style={{color: `#FFFFFF`}}>Température :</h3><p className='texte' style={{ color: '#FFFFFF',animationDelay: '0s' }}> {weather.main.temp}°C</p></li>
+            <li><h3 style={{color: `#FFFFFF`}}>Ressentie :</h3><p className='texte' style={{ color: '#FFFFFF', animationDelay: '1s' }}> {weather.main.feels_like}°C</p></li>
+            <li><h3 style={{color: `#FFFFFF`}}>Humidité :</h3><p className='texte' style={{ color: '#FFFFFF', animationDelay: '2s' }}> {weather.main.humidity}%</p></li>
+            <li><h3 style={{color: `#FFFFFF`}}>Conditions :</h3><p className='texte' style={{ color: '#FFFFFF', animationDelay: '3s' }}> {weather.weather[0].main} ({weather.weather[0].description})</p></li>
+           
+            <li><h3 style={{color: `#FFFFFF`}}>Vent :</h3><p className='texte' style={{ color: '#FFFFFF', animationDelay: '6s' }}> {weather.wind.speed} m/s</p></li>
+            <li><h3 style={{color: `#FFFFFF`}}>Latitude :</h3><p className='texte' style={{ color: '#FFFFFF', animationDelay: '7s' }}> {weather.coord.lat}</p></li>
+            <li><h3 style={{color: `#FFFFFF`}}>Longitude :</h3><p className='texte' style={{ color: '#FFFFFF', animationDelay: '7s' }}> {weather.coord.lon}</p></li>
+            </ol>
+             
+            
+           </div>
             ) : (
               <p>{error}</p>
             )}
+            
+            
           </div>
         </div>
       )}
