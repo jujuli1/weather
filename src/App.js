4ce1c0ae7ae5c5fbeb8b5e17c2e72966a7d5cf0d
LEFEVRE <lefevrejulien@live.fr>
2024-12-09 13:42:01 +0100

    9dec24

--- src/App.js
+++ src/App.js
@@ -50,86 +50,7 @@ function App() {
       });
   };
 
-  // Effet pour initialiser la scène Three.js
-  useEffect(() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-    
-    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById("three-container").appendChild(renderer.domElement);
-
-    // Moon
-    const geometry = new THREE.SphereGeometry(0.3, 32, 32)
-    const texture = new THREE.TextureLoader().load('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5MQZS7B0LbbZ4LJgK-djWwW3oczycATcJ3w&s', () => {
-      console.log("Texture chargée avec succès");
-  }, undefined, (err) => {
-      console.error("Erreur lors du chargement de la texture :", err);
-  });
-    const material = new THREE.MeshPhongMaterial({map: texture})
-    const cube = new THREE.Mesh(geometry, material);
-    const ambientLight = new THREE.AmbientLight(0xffffff,0.5);
-    scene.add(ambientLight);
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(5, 10, 7.5);
-    scene.add(directionalLight);
-    scene.add(cube);
-    
-
-    // Caméra
-    camera.position.z = 5;
-
-
-    // Variables pour le mouvement circulaire
-    let angle = 0; // Angle initial en radians
-    const radius = 4; // Rayon du cercle
-    const speed = 0.001; // Vitesse de rotation (radians par frame)
-
-    // Animation
-    function animateRotate() {
-        // Calcul des nouvelles coordonnées
-        angle += speed; // Incrémenter l'angle à chaque frame
-        const x = radius * Math.cos(angle); // Calcul de la position x
-        const y = radius * Math.sin(angle); // Calcul de la position y
-
-        cube.position.set(x, y, 0); // Mettre à jour la position de la sphère
-
-        renderer.render(scene, camera);
-    }
-
-    renderer.setAnimationLoop(animateRotate);
-
-    // Animation
-    const animate = () => {
-      cube.rotation.x += 0.002;
-      cube.rotation.y += 0.002;
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
-    };
-    animate();
-
-    // Cycle de luminosité
-    let increasing = true; // Variable pour suivre la direction (augmentation ou diminution)
-
-    setInterval(() => {
-        if (increasing) {
-          ambientLight.intensity = Math.max(ambientLight.intensity - 0.3, 0); // Diminue jusqu'à 0
-            if (ambientLight.intensity === 0) increasing = false; // Inverse la direction
-            
-        } else {
-          ambientLight.intensity = Math.min(ambientLight.intensity + 1, 4); // Augmente jusqu'à 4
-            if (ambientLight.intensity === 4) increasing = true; // Inverse la direction
-            
-        }
-        console.log("Intensité actuelle de la lumière :", ambientLight.intensity);
-    }, 4500);
-
-    // Nettoyer la scène lors du démontage
-    return () => {
-      document.getElementById("three-container").removeChild(renderer.domElement);
-    };
-  }, []);
+  
 
   return (
     <div className="App">
@@ -168,7 +89,7 @@ function App() {
           onChange={(e) => setSearch(e.target.value)}
         />
         <button className="btnR" onClick={searchPressed}>
-          Meteok
+          Search
         </button>
       </header>
 
@@ -213,8 +134,7 @@ function App() {
         </div>
       )}
 
-      {/* Conteneur pour le rendu Three.js */}
-      <div id="three-container" ></div>
+    
     </div>
   );
 }
